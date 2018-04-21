f3edeb88495fd593b249efa1e30fbd78150b62d4
Divya Khabiya <dkhabiya@hawk.iit.edu>
2018-04-21 23:11:21 +0000

    Added views, templates.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Activity
 
 # Create your views here.
+def activityList(request):
+    activities = Activity.objects.filter(dateCreated__lte=timezone.now()).order_by('dateCreated')
+    return render(request, 'app/activityList.html', {'activities': activities})
